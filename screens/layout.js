885ba6a51dbd6d4ea184c51a3360617981b41678
troqapp <anakinwasgood@gmail.com>
2025-10-25 15:53:37 +0530

    Create layout.js

---
+++ screens/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Weland - Your Digital Identity',
+  description: 'Create your digital identity and join the virtual nation',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="font-sans">{children}</body>
+    </html>
+  )
+}
